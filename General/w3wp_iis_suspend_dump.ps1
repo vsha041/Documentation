2a842a583aba6834c716137f2dbdc1844814b3be
Varun Sharma <varun.varunsharma@gmail.com>
2024-08-01 18:08:21 +1200

    Create w3wp_iis_suspend_dump.ps1

---
+++ General/w3wp_iis_suspend_dump.ps1
@@ -0,0 +1,10 @@
+#https://stackoverflow.com/questions/39936971/procdump-error-writing-dump-file-0x80070005-error-0x80070005-2147024891-acc
+#Prevent IIS from recycling the process during procdump and causing an Access Denied error message
+$iispid = Get-Process svchost | ?{$_.modules.ModuleName -eq "iisw3adm.dll"} | Select -First 1 -ExpandProperty Id
+$workerpid = Get-Process w3wp | Sort ws -Descending | Select -First 1 -ExpandProperty Id
+#move to location where you want to save the dump files
+#Add -accepteula to the sysinternals calls if you want to bypass the initial EULA prompt on new servers
+& "C:\Temp\PSTools\pssuspend.exe" $iispid 
+Write-Output "Creating memory dump for w3wp PID $workerpid"
+& "C:\Temp\Procdump\procdump.exe" -ma $workerpid
+& "C:\Temp\PSTools\pssuspend.exe" $iispid -r
